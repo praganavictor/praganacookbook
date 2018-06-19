c6ad6ba62a47a07fd5552526246aa20fced653ff
Victor Pragana <victormerigue@gmail.com>
2018-06-19 20:05:15 -0300

    User register recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,21 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    @recipe.save
+    redirect_to @recipe
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+      :cook_time, :ingredients, :method)
+  end
 end
