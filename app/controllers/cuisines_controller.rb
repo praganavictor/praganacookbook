17db2b06f98492d2688f1764cc03e2047651b5c4
Victor Pragana <victormerigue@gmail.com>
2018-06-19 23:07:56 -0300

    Admin register cuisines

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -3,4 +3,23 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.find(params[:id])
     @recipe = Recipe.where(:cuisine => params[:id])
   end
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(cuisine_params)
+
+    if @cuisine.save
+      redirect_to @cuisine
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def cuisine_params
+    params.require(:cuisine).permit(:name)
+
+  end
 end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,4 @@
 class Cuisine < ApplicationRecord
   has_many :recipes
+  validates :name, presence: { message: 'Você deve informar o nome da cozinha'}
 end
