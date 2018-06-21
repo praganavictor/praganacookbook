51dd70504a7c92b8db286402e633cc0c7ebb7036
Victor Pragana <victormerigue@gmail.com>
2018-06-20 22:59:46 -0300

    user needs register recipes correctly

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,8 +10,13 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
 
-    @recipe.save
-    redirect_to @recipe
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+
+
   end
 
   private
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :cuisine
+  validates :title, :recipe_type, :cuisine_id, :difficulty, :cook_time,
+  :ingredients, :method, presence: {
+    message: 'Você deve informar todos os dados da receita'}
 end
