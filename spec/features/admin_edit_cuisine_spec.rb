12b26866aaac93261781719ca4f0f68435a4b495
Victor Pragana <victormerigue@gmail.com>
2018-07-04 11:41:35 -0300

    Admin have to fill all fields on form

--- spec/features/admin_edit_cuisine_spec.rb
+++ spec/features/admin_edit_cuisine_spec.rb
@@ -13,5 +13,13 @@ feature 'Admin edit a cuisine' do
 
     expect(page).to have_css('h1', text: 'Espanhola')
   end
+  scenario 'admin have to fill name' do
+    visit root_path
+    click_on 'Criar cozinha'
 
+    fill_in 'Nome', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_css('li', text: 'Você deve informar o nome da cozinha')
+  end
 end
