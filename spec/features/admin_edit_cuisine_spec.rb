7d073d69eee8fddd737ba83017db227f4d574b53
Victor Pragana <victormerigue@gmail.com>
2018-07-04 11:52:41 -0300

    Cuisine has to be unique

--- spec/features/admin_edit_cuisine_spec.rb
+++ spec/features/admin_edit_cuisine_spec.rb
@@ -13,7 +13,8 @@ feature 'Admin edit a cuisine' do
 
     expect(page).to have_css('h1', text: 'Espanhola')
   end
-  scenario 'admin have to fill name' do
+
+  scenario 'and have to fill name' do
     visit root_path
     click_on 'Criar cozinha'
 
@@ -21,5 +22,17 @@ feature 'Admin edit a cuisine' do
     click_on 'Enviar'
 
     expect(page).to have_css('li', text: 'Você deve informar o nome da cozinha')
+  end
+
+  scenario 'and cuisine has to be unique' do
+    Cuisine.create(name: 'Brasileira')
+
+    visit root_path
+    click_on 'Criar cozinha'
+
+    fill_in 'Nome', with: 'Brasileira'
+    click_on 'Enviar'
+
+    expect(page).to have_css('li', text: 'Essa cozinha ja existe')
   end
 end
